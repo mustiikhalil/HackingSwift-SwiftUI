d040d1796295741d535433f050a838ed3342859e
mustii <mustii@mmk.one>
2019-12-13 22:55:28 +0300

    Added new swiftui example app

---
+++ Betterrest/Betterrest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Betterrest
+//
+//  Created by Mustafa Khalil on 12/13/19.
+//  Copyright © 2019 Mustafa Khalil. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
